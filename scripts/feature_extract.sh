4bd98793a9324f7f047077f0450418e611b7fe53
worstchan <1029713857@qq.com>
2025-05-03 13:43:21 +0000

    feat: update huggingface support for feature extraction

--- scripts/feature_extract.sh
+++ scripts/feature_extract.sh
@@ -2,14 +2,19 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
+FRAMEWORK="huggingface"     # "fairseq" or "huggingface"
+CHECKPOINT_DIR="worstchan/EAT-base_epoch30_finetune_AS2M"  # Path to the checkpoint file ( xxx.pt for fairseq, repo id for huggingface)
+GRANULARITY="all"        # "all", "frame", or "utterance"
+
 python EAT/feature_extract/feature_extract.py  \
     --source_file='EAT/feature_extract/test.wav' \
     --target_file='EAT/feature_extract/test.npy' \
     --model_dir='EAT' \
-    --checkpoint_dir='/hpc_stor03/sjtu_home/wenxi.chen/model_ckpt/EAT/checkpoint10.pt' \
-    --granularity='frame' \
+    --checkpoint_dir=$CHECKPOINT_DIR \
+    --granularity=$GRANULARITY \
     --target_length=1024 \
-    --mode='pretrain'
+    --mode='finetune' \
+    --framework=$FRAMEWORK \
 
 # For optimal performance, 1024 is recommended for 10-second audio clips. (128 for 1-second)
 # You should adjust the target_length parameter based on the duration and characteristics of your specific audio inputs.
@@ -20,3 +25,4 @@ python EAT/feature_extract/feature_extract.py  \
 # utterance: only the cls token feature.
 
 # 2 mode of checkpoints: (EAT) pretrain or (EAT) finetune
+# 2 framework: fairseq or huggingface
